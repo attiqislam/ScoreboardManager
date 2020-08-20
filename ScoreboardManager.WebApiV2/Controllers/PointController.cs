ba01b8be98fe70027e394c5666acd7a62b72d83d
Attiq-ul Islam <attiq@gmail.com>
2020-08-20 18:04:05 +0300

    Added base and other 3 controllers with get all api methods.

---
+++ ScoreboardManager.WebApiV2/Controllers/PointController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ScoreboardManager.Common.ViewModels;
+using ScoreboardManager.ServiceFacade;
+
+namespace ScoreboardManager.WebApiV2.Controllers
+{
+    public class PointController : BaseController
+    {
+        [Route("Points/GetAll")]
+        public List<PointViewModel> PostPointsGet()
+        {
+            string errorMessage = string.Empty;
+            Facade facade = new Facade();
+            return facade.PointsGetAll(out errorMessage);
+        }
+    }
+}
